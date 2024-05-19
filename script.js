7fffef8c310590e7c5f81270ff9c4ae0e66e4ae7
Davi <davipadecarvalho@hotmail.com>
2024-05-19 20:50:01 -0300

    commit

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function expand(element) {
-    element.classList.toggle("expand");
+function sendEmail() {
+
+    //var name = document.getElementById("name").value;
+    var resposta = document.getElementById("anel").value;
+    const email = "davipadecarvalho@homail.com";
+
+    Email.send({
+        SecureToken : token,
+        To : 'davipiresaquino@gmail.com',
+        From : "davipadecarvalho@hotmail.com",
+        Subject : "This is the subject",
+        Body : resposta
+    }).then(
+      message => alert(message)
+    );
+
 }
