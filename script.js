106b692456dd4ea940174ad9dbe4217791014c20
Davi <davipadecarvalho@hotmail.com>
2024-05-19 21:01:18 -0300

    commit

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ function sendEmail() {
     //var name = document.getElementById("name").value;
     var resposta = document.getElementById("anel").value;
     const email = "davipadecarvalho@homail.com";
-    alert("teste");
+    const token = "8c48cd89-f44b-414a-885e-127c7e4bafe7";
+
     Email.send({
         SecureToken : token,
         To : 'davipiresaquino@gmail.com',
