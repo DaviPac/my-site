aa71d1ea8fc411c179cccef1ed0516aa9e9d3bfe
Davi <131999650+DaviPac@users.noreply.github.com>
2025-07-08 11:17:02 -0300

    fix auth

--- login.js
+++ login.js
@@ -1,21 +1,22 @@
 document.getElementById('loginForm').addEventListener('submit', async function(e) {
-      e.preventDefault();
+    e.preventDefault();
 
-      const username = document.getElementById('username').value;
-      const password = document.getElementById('password').value;
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
-      const response = await fetch('https://testesitebackend.fly.dev/login', {
+    const response = await fetch('https://testesitebackend.fly.dev/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username, password })
-      });
-
-      if (response.status === 401) {
-        document.getElementById('resultado').textContent = '❌ Usuário ou senha incorretos.';
-      } else if (response.ok) {
-        const data = await response.json();
-        document.getElementById('resultado').textContent = '✅ Login bem-sucedido!';
-      } else {
-        document.getElementById('resultado').textContent = '⚠️ Erro ao conectar com o servidor.';
-        }
     });
+
+    if (response.status === 401) {
+        document.getElementById('resultado').textContent = '❌ Usuário ou senha incorretos.';
+    } else if (response.ok) {
+        const data = await response.json();
+        localStorage.setItem('token', data.token);
+        window.location.href('index.html');
+    } else {
+        document.getElementById('resultado').textContent = '⚠️ Erro ao conectar com o servidor.';
+    }
+});
