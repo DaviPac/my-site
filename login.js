17d889749fde64d516fd47d53e19ba4010caf51e
Davi <131999650+DaviPac@users.noreply.github.com>
2025-07-08 10:54:47 -0300

    arquivos .js

---
+++ login.js
@@ -0,0 +1,21 @@
+document.getElementById('loginForm').addEventListener('submit', async function(e) {
+      e.preventDefault();
+
+      const username = document.getElementById('username').value;
+      const password = document.getElementById('password').value;
+
+      const response = await fetch('https://testesitebackend.fly.dev/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username, password })
+      });
+
+      if (response.status === 401) {
+        document.getElementById('resultado').textContent = '❌ Usuário ou senha incorretos.';
+      } else if (response.ok) {
+        const data = await response.json();
+        document.getElementById('resultado').textContent = '✅ Login bem-sucedido!';
+      } else {
+        document.getElementById('resultado').textContent = '⚠️ Erro ao conectar com o servidor.';
+        }
+    });
---
+++ register.js
@@ -0,0 +1,21 @@
+document.getElementById('registerForm').addEventListener('submit', async function(e) {
+      e.preventDefault();
+
+      const username = document.getElementById('username').value;
+      const password = document.getElementById('password').value;
+
+      const response = await fetch('https://testesitebackend.fly.dev/register', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username, password })
+      });
+
+      if (response.status === 401) {
+        document.getElementById('resultado').textContent = '❌ Usuário já existe.';
+      } else if (response.ok) {
+        const data = await response.json();
+        document.getElementById('resultado').textContent = '✅ Registro bem-sucedido!';
+      } else {
+        document.getElementById('resultado').textContent = '⚠️ Erro ao conectar com o servidor.';
+        }
+    });
